d37f37f24eee8776fd34b12a0212277e2e6665cd
Abbas Gheydi <abbas.gheydi@gmail.com>
2024-12-06 20:13:31 +0330

    Fix issue with setting wallpaper on Darwin (macOS)

--- binggo.go
+++ binggo.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
 	"strings"
 	"time"
 
@@ -15,36 +17,55 @@ import (
 
 const (
 	bingAddress    string = "https://www.bing.com/"
-	bingApiAddress string = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
+	bingApiAddress string = bingAddress + "HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
+	Green                 = "\033[32m"
+	Red                   = "\033[31m"
+	ResetColor            = "\033[0m"
 )
 
 func main() {
 
 	imageUrl, imageName, err := get_image_name_url()
 	if err != nil {
-		log.Fatal(err)
+		logRedAndExit(err.Error())
 
 	}
 
-	log.Println(imageName)
+	logGreen("this image will be sets as s wallpaper: " + imageName)
 
 	if strings.Contains(os.Getenv("XDG_DATA_DIRS"), "xfce") {
 		wallpaper.Desktop = "XFCE"
 
 	}
-	err = wallpaper.SetFromURL(imageUrl)
-	if err != nil {
-		log.Fatal(err)
+	// Check if the OS is Darwin (macOS)
+	if runtime.GOOS == "darwin" {
+		if err := exec.Command("osascript", "-e", `tell application "System Events" to tell every desktop to set picture to "DEFAULT"`).Run(); err != nil {
+			logRedAndExit("could not set wallpaper to default" + err.Error())
+		}
 
 	}
 
+	err = wallpaper.SetFromURL(imageUrl)
+	if err != nil {
+		logRedAndExit(err.Error())
+	}
+	logGreen("the wallpaper is set to: " + imageName)
+
+}
+
+func logRedAndExit(message string) {
+	log.Println(Red + message + ResetColor)
+	os.Exit(1)
+}
+
+func logGreen(message string) {
+	log.Println(Green + message + ResetColor)
 }
 
 func downloadJson() (json []byte, err error) {
 	curl := http.Client{Timeout: time.Second * 10}
 	resp, err := curl.Get(bingApiAddress)
 	if err != nil {
-		log.Println("could not get url")
 		return
 	}
 	defer resp.Body.Close()
@@ -77,13 +98,11 @@ func (b Bing_Response) receive() (res Bing_Response, err error) {
 
 	data, err := downloadJson()
 	if err != nil {
-		log.Println(err)
 		return
 	}
 
 	err = json.Unmarshal(data, &res)
 	if err != nil {
-		log.Println(err)
 		return
 	}
 
